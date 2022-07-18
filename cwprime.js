702181e5d02ee43bb50a136f3ac9ad4a49cc1762
kiranpp1234 <107753857+kiranpp1234@users.noreply.github.com>
2022-07-18 15:21:09 +0530

    Add files via upload

---
+++ cwprime.js
@@ -0,0 +1,12 @@
+const num = prompt("Enter a number:")
+function isPrime(num) {
+
+
+   if (num === 2 || num % 2 === 1) {
+      console.log("the num is prime")
+   }
+   else {
+      console.log("the num is not prime")
+   }
+
+}
